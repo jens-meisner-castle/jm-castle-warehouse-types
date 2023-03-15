ec8d884bade8cc00cea62a2ba673b6a45ca8f002
Jens Meisner <jnsmsnr@gmail.com>
2023-03-15 15:24:15 +0100

    bugfix: missing tables in export

--- src/database/Table.ts
+++ src/database/Table.ts
@@ -1,3 +1,20 @@
+import {
+  Row_Article,
+  Row_Attribute,
+  Row_Costunit,
+  Row_Emission,
+  Row_EmissionRequest,
+  Row_Hashtag,
+  Row_ImageContent,
+  Row_ImageReference,
+  Row_Manufacturer,
+  Row_Receipt,
+  Row_ReceiptRequest,
+  Row_Receiver,
+  Row_Store,
+  Row_StoreSection,
+} from "../row/index.js";
+
 export const AllTableNames = {
   article: "Artikel",
   store: "Lager",
@@ -16,3 +33,20 @@ export const AllTableNames = {
 };
 
 export type TableName = keyof typeof AllTableNames;
+
+export type TableRows = {
+  article: Row_Article;
+  store: Row_Store;
+  store_section: Row_StoreSection;
+  costunit: Row_Costunit;
+  hashtag: Row_Hashtag;
+  attribute: Row_Attribute;
+  receiver: Row_Receiver;
+  manufacturer: Row_Manufacturer;
+  image_content: Row_ImageContent;
+  image_reference: Row_ImageReference;
+  receipt: Row_Receipt;
+  emission: Row_Emission;
+  receipt_request: Row_ReceiptRequest;
+  emission_request: Row_EmissionRequest;
+};
